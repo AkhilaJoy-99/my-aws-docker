3aa118aacb4cae6bbf8db61dc8a2e8f4feb901c7
AkhilaJoy-99 <136692934+AkhilaJoy-99@users.noreply.github.com>
2023-07-26 12:22:57 +0530

    Add files via upload

---
+++ auth-server/src/main/java/com/mysmartshop/authserver/api/HomeController.java
@@ -0,0 +1,21 @@
+package com.mysmartshop.authserver.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	@GetMapping("/message")
+	public String greet() {
+		return "Hello from us";
+	}
+	@GetMapping("/user")
+	public String greetUser() {
+		return "Hello user";
+	}
+	@GetMapping("/admin")
+	public String greetAdmin() {
+		return "Hello admin";
+	}
+
+}
